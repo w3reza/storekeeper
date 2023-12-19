94ad79bd78282166b460e2ff1d17e58581fb72cc
w3reza <w3reza.ju@gmail.com>
2023-12-19 12:27:53 +0600

    Auth-Deleted

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProfileController;
-
 use App\Http\Controllers\Backend\SaleController;
 use App\Http\Controllers\Backend\backendController;
 use App\Http\Controllers\Backend\productController;
@@ -19,39 +17,31 @@ use App\Http\Controllers\Backend\productController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', [backendController::class, 'index'])->name('home');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::prefix('admin')->group(function () {
+    Route::get('/', [backendController::class, 'index'])->name('home');
+/*
+|--------------------------------------------------------------------------
+| Admin Section product Routes
+|--------------------------------------------------------------------------
+*/
 
-    Route::prefix('admin')->group(function () {
-        Route::get('/', [backendController::class, 'index'])->name('home');
-    /*
-    |--------------------------------------------------------------------------
-    | Admin Section product Routes
-    |--------------------------------------------------------------------------
-    */
+Route::get('/products', [productController::class, 'show'])->name('products.show');
+Route::get('product/create', [productController::class, 'create'])->name('product.create');
+Route::post('product/store', [productController::class, 'store'])->name('product.store');
+Route::get('product/edit/{id}', [productController::class, 'edit'])->name('product.edit');
+Route::post('product/{id}', [productController::class, 'update'])->name('product.update');
+Route::get('product/{id}', [productController::class, 'destroy'])->name('product.delete');
 
-    Route::get('/products', [productController::class, 'show'])->name('products.show');
-    Route::get('product/create', [productController::class, 'create'])->name('product.create');
-    Route::post('product/store', [productController::class, 'store'])->name('product.store');
-    Route::get('product/edit/{id}', [productController::class, 'edit'])->name('product.edit');
-    Route::post('product/{id}', [productController::class, 'update'])->name('product.update');
-    Route::get('product/{id}', [productController::class, 'destroy'])->name('product.delete');
-
-    Route::get('/sales', [SaleController::class, 'index'])->name('sales.show');
-    Route::post('sale/store', [SaleController::class, 'store'])->name('sale.store');
+Route::get('/sales', [SaleController::class, 'index'])->name('sales.show');
+Route::post('sale/store', [SaleController::class, 'store'])->name('sale.store');
 
 
-    });
 });
 
 //Routing Group without Middleware
@@ -69,7 +59,3 @@ Route::middleware('auth')->group(function () {
 | End of Admin Web Routes with admin prefix
 |--------------------------------------------------------------------------
 */
-
-
-
-require __DIR__.'/auth.php';
